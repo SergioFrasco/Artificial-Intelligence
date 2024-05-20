1c78fd2b380b319dfd831c42b22990ad5b7c504e
Tristan Dos Remendos <tristan.remendos@gmail.com>
2024-05-20 16:13:17 +0200

    Added commentary and change prints

--- Project/ImprovedAgent.py
+++ Project/ImprovedAgent.py
@@ -14,7 +14,7 @@ STATE_LIMIT = 100000
 
 class ImprovedAgent(Player):
     """
-    TroutBot uses the Stockfish chess engine to choose moves. In order to run TroutBot you'll need to download
+    ImprovedAgent uses the Stockfish chess engine to choose moves. In order to run this agent you'll need to download
     Stockfish from https://stockfishchess.org/download/ and create an environment variable called STOCKFISH_EXECUTABLE
     that is the path to the downloaded Stockfish executable.
     """
@@ -25,7 +25,7 @@ class ImprovedAgent(Player):
         self.my_piece_captured_square = None
         self.move_number = 0
 
-        # Only play knight push when we're white
+        # only play knight push when we're white
         self.white_move = [chess.Move.from_uci("b1c3")]
         self.white_move.append(chess.Move.from_uci("c3b5"))
         self.white_move.append(chess.Move.from_uci("b5d6"))
@@ -55,33 +55,26 @@ class ImprovedAgent(Player):
         
         self.my_piece_captured_square = capture_square
 
-        # if the opponent captured our piece, remove it from all board states
         new_states = set()
-        # if captured_my_piece:
-        #     for state in self.board_states:
-        #         board = chess.Board(state)
-        #         board.remove_piece_at(capture_square)
-        #         new_states.add(board.fen())
-        #     self.board_states = new_states
-
-        # instead if an opponent captured our piece, we should run through all the boards, make all possible moves where that move was possible and update the boards
+        # if an opponent captured our piece, we should run through all the boards, make all possible moves where that move was possible and update the boards
         if captured_my_piece:
             for state in self.board_states:
                 board = chess.Board(state)
                 
-                # Find all possible moves where the capture was possible
+                # find all possible moves where the capture was possible, only add those moves
                 for move in board.pseudo_legal_moves:
                     if move.to_square == capture_square:
                         board.push(move)
                         new_states.add(board.fen())
                         board.pop()
         else:
+            # otherwise, just copy the current set of possible boards
             new_states = self.board_states.copy()
         
         generated_states = set()
-        for state in new_states:    #changed this from board states to new states so it takes into account the changes above if there were any
+        # generate all possible moves for each state, this now becomes our new set of possible states
+        for state in new_states: 
             board = chess.Board(state)
-            # print(board)
             board.turn = not self.color
             for move in board.pseudo_legal_moves: 
                     if (len(generated_states) > STATE_LIMIT):
@@ -95,72 +88,57 @@ class ImprovedAgent(Player):
                 break
         
         self.board_states = generated_states
-        # for state in generated_states:
-        #     # print(chess.Board(state))
-        #     self.board_states.add(state)
 
     def naive_entropy(self, sense_actions: List[Square]):
-        """
-        Implements the Naive Entropy sensing strategy.
-        Calculates the entropy for each square on the board and selects the center square
-        of the 3x3 region with the highest aggregate entropy.
-        """
-        # Cache board objects for each state
-        board_cache = {state: chess.Board(state) for state in self.board_states}
+        set_of_board_objects = {state: chess.Board(state) for state in self.board_states}
 
-        # Calculate entropy for each square
-        square_entropies = {}
+        # determine entropy per square
+        entropy_per_square = {}
         for square in range(64):
-            piece_counts = Counter(board_cache[state].piece_at(square) for state in self.board_states)
-            total = sum(piece_counts.values())
-            entropy = -sum(count/total * math.log2(count/total) if count > 0 else 0 for count in piece_counts.values())
-            square_entropies[square] = entropy
+            count_per_piece = Counter(set_of_board_objects[state].piece_at(square) for state in self.board_states)
+            total_count_on_square = sum(count_per_piece.values())
 
-        # Compute aggregate entropy for each 3x3 region
-        region_entropies = {}
-        for center_square in sense_actions:
-            region = [center_square - 9, center_square - 8, center_square - 7,
-                    center_square - 1, center_square, center_square + 1,
-                    center_square + 7, center_square + 8, center_square + 9]
-            region_entropy = sum(square_entropies[square] for square in region)
+            entropy = -sum(count/total_count_on_square * math.log2(count/total_count_on_square) if count > 0 else 0 for count in count_per_piece.values())
             
-            region_entropies[center_square] = region_entropy 
+            entropy_per_square[square] = entropy
 
-        best_square = max(region_entropies, key=region_entropies.get)
+        # determine entropy per 9x9 area
+        entropy_per_region = {}
+        for middle_square in sense_actions:
+            area = [middle_square - 9, middle_square - 8, middle_square - 7,
+                    middle_square - 1, middle_square, middle_square + 1,
+                    middle_square + 7, middle_square + 8, middle_square + 9]
+            
+            entropy_per_region[middle_square] = sum(entropy_per_square[square] for square in area)
 
-        print(f"----ENTROPY Orginal Best Sense was: {chess.square_name(best_square)}----")
-            # Adjust the sensing square if it is along the borders
-        rank, file = divmod(best_square, 8)
-        if rank == 0:  # Bottom border
-            best_square += 8  # Move one square up
-        elif rank == 7:  # Top border
-            best_square -= 8  # Move one square down
-        elif file == 0:  # Left border
-            best_square += 1  # Move one square right
-        elif file == 7:  # Right border
-            best_square -= 1  # Move one square left
-        print(f"----ENTROPY Best sense after adjustments: {chess.square_name(best_square)}----")   
+        uncertainty_minimizer = max(entropy_per_region, key=entropy_per_region.get)
+
+        print(f"----ENTROPY Orginal Best Sense was: {chess.square_name(uncertainty_minimizer)}----")
         
-        return best_square
+        # padding around the edges in case we accidentally sense an edge somehow
+        rank, file = divmod(uncertainty_minimizer, 8)
+        if rank == 0:  # bottom border
+            uncertainty_minimizer += 8  # move one square up
+        elif rank == 7:  # top border
+            uncertainty_minimizer -= 8  # move one square down
+        elif file == 0:  # left border
+            uncertainty_minimizer += 1  # move one square right
+        elif file == 7:  # right border
+            uncertainty_minimizer -= 1  # move one square left
+        print(f"----ENTROPY Best sense after adjustments: {chess.square_name(uncertainty_minimizer)}----")   
+        
+        # return option that minimized uncertainty
+        return uncertainty_minimizer
 
     def use_stockfish(self, sense_actions: List[Square]):
+        # limit number of states if there are too many states to sense with
         if len(self.board_states) > 0: 
             if (len(self.board_states) > STATE_LIMIT):
                 self.board_states = random.sample(self.board_states, STATE_LIMIT)
 
             time_limit = 10 / len(self.board_states) 
-        # else:
-        #     # print("Sensed the centre")
-        #     central_squares = [
-        #         chess.D4, chess.E4, chess.D5, chess.E5,
-        #         chess.C4, chess.F4, chess.C5, chess.F5,
-        #         chess.C3, chess.D3, chess.E3, chess.F3,
-        #         chess.C6, chess.D6, chess.E6, chess.F6
-        #     ]
-        #     for square in central_squares:
-        #         if square in sense_actions:
-        #             return square
 
+        # go through each possible board state
         ideal_senses = dict()
         for state in self.board_states:
             board = chess.Board(state)
@@ -169,41 +147,43 @@ class ImprovedAgent(Player):
             if board.is_valid():
                 
                 try:
+                    # determine stockfish's best move as the opponent
                     result = self.engine.play(board, chess.engine.Limit(time=time_limit))
                     
                     if result.move is not None:
                         if (result.move.to_square in sense_actions):
+                            # count the moves across each board state
                             if result.move.to_square in ideal_senses.keys():
                                 ideal_senses[result.move.to_square] += 1
                             else:
                                 ideal_senses[result.move.to_square] = 1
                 except (chess.engine.EngineError, chess.engine.EngineTerminatedError) as e:
-                    # print('Engine bad state at "{}"'.format(board.fen()))
-                    # print(board)
                     pass
-                # print(result.move.to_square) 
-        # print(len(self.board_states))
 
         if (len(ideal_senses.values()) == 0):
             print(f"STOCKFISH DIED, chose random")
             return random.choice(sense_actions)
 
+        # get the most popular moves across each state, we break ties by just picking the first move in the list
         max_count = max(ideal_senses.values())
         best_senses = [move for move, count in ideal_senses.items() if count == max_count]
 
+        # padding the sense result so we don't waste possible information gain
         if best_senses:
-            # print(f"Number of moves: {len(best_senses)}")
+
             print(f"----STOCKFISH Orginal Best Sense was: {chess.square_name(best_senses[0])}----")
-            # Adjust the sensing square if it is along the borders
+
+            # adjust the sensing square if it is along the borders
             rank, file = divmod(best_senses[0], 8)
-            if rank == 0:  # Bottom border
-                best_senses[0] += 8  # Move one square up
-            elif rank == 7:  # Top border
-                best_senses[0] -= 8  # Move one square down
-            elif file == 0:  # Left border
-                best_senses[0] += 1  # Move one square right
-            elif file == 7:  # Right border
-                best_senses[0] -= 1  # Move one square left
+            if rank == 0:  # bottom border
+                best_senses[0] += 8  # move one square up
+            elif rank == 7:  # top border
+                best_senses[0] -= 8  # move one square down
+            elif file == 0:  # left border
+                best_senses[0] += 1  # move one square right
+            elif file == 7:  # right border
+                best_senses[0] -= 1  # move one square left
+
             print(f"----STOCKFISH Best sense after adjustments: {chess.square_name(best_senses[0])}----")   
 
             return best_senses[0]
@@ -215,7 +195,7 @@ class ImprovedAgent(Player):
             Optional[Square]:
         print(f"In choose_sense {len(self.board_states)}")
 
-        # Always make these the first senses as stockfish is most likely to play these moves first
+        # always make these the first senses as stockfish is most likely to play these moves first
         if self.color == chess.WHITE:
             if self.move_number == 1:
                 print("preset sense e6")
@@ -230,18 +210,17 @@ class ImprovedAgent(Player):
             print("-----My piece was just captrured-----")
             return self.my_piece_captured_square
 
-        # if we might capture a piece when we move, sense where the capture will occur
-        # future_move = self.choose_move(move_actions, seconds_left)
-        # if future_move is not None and any(chess.Board(board).piece_at(future_move.to_square) is not None for board in self.board_states):
-        #     return future_move.to_square
-
+        # if the board state is perfectly known, use stockfish to sense the best square
         if len(self.board_states) == 1:
             return self.use_stockfish(sense_actions)
-
+    
+        # edges of the board being removed as possible senseing actions
         edges = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 16, 23, 24, 31, 32, 39, 40, 47, 48, 55, 56, 57, 58, 59, 60, 61, 62, 63])
         sense_actions = np.setdiff1d(sense_actions, edges)
         sense_actions = sense_actions.tolist()
 
+        # if there are multiple board states, use naive entropy to find the best sense to minimize
+        # uncertainty about the board
         sense_square_using_naive_entropy = self.naive_entropy(sense_actions)
 
         return sense_square_using_naive_entropy
@@ -252,18 +231,17 @@ class ImprovedAgent(Player):
         # create a new set of board states based on the sense result
         new_board_states = set()
 
+        # remove any board states that aren't consistent with what was seen in the window
         for state in self.board_states:
             board = chess.Board(state)
             flag = True
             for square, piece in sense_result:
-                # print(board.piece_at(square), piece) 
+
                 if board.piece_at(square) != piece:
                     flag = False
                     break
 
             if flag:
-                # print("BOARD WAS VALIDDDD")
-                # print(board)
                 new_board_states.add(board.fen())
 
         self.board_states = new_board_states
@@ -271,32 +249,32 @@ class ImprovedAgent(Player):
     def choose_move(self, move_actions: List[chess.Move], seconds_left: float) -> Optional[chess.Move]:
         print(f"In choose_move {len(self.board_states)}")
 
-
-
-      # If playing as white, execute the predefined rush moves
+        # if playing as white, execute the predefined rush moves
         if self.color == chess.WHITE and self.move_number < len(self.white_move):
             if self.white_move[self.move_number] in move_actions: #make sure the knight is still alive on the final move
                 print("Attempting to kill king with move: ", self.move_number)
                 return self.white_move[self.move_number]
         
-
-        # Limit the number of possible states
+        # limit the number of possible states if it is too high
         if len(self.board_states) > STATE_LIMIT:
             self.board_states = random.sample(self.board_states, STATE_LIMIT)
 
+        # use the number of boards to set stockfish's time limit
         if len(self.board_states) > 0:
             time_limit = 10 / len(self.board_states)
-        else:
+
+        # if we run out of board states for some reason, just pick a random move
+        else: 
             return random.choice(move_actions)
         
-        # Check if the king is under attack
+        # check if the king is under attack in any boards
         for state in self.board_states:
             board = chess.Board(state)
             king_square = board.king(self.color)
             if king_square:
                 attackers = board.attackers(not self.color, king_square)
                 if attackers:
-                    # King is under attack, find the best move to avoid the attack
+                    # king is under attack, find the best move to avoid the attack
                     best_move = None
                     best_score = -float('inf')
                     for move in move_actions:
@@ -305,15 +283,17 @@ class ImprovedAgent(Player):
                             new_board.push(move)
                             if not new_board.is_attacked_by(not self.color, king_square):
                                 try:
+                                    # use stockfish to find the best move to play such that we save the king
                                     result = self.engine.play(new_board, chess.engine.Limit(time=time_limit))
                                     score = result.info.get('score', None)
                                     if score is not None and score.white().score(mate_score=32000) > best_score:
                                         best_move = move
                                         best_score = score.white().score(mate_score=32000)
-                                    print("*Evasive maneuvre to get the king out of check*")
                                 except (chess.engine.EngineTerminatedError, chess.engine.EngineError):
                                     pass
                     if best_move != None:
+                        print("*Evasive maneuvre to get the king out of check*")
+                        # whichever move across each possible state that yielded the best position for us after is returned
                         return best_move
 
         # if we might be able to take the king, try to
@@ -326,14 +306,16 @@ class ImprovedAgent(Player):
                 if enemy_king_attackers:
                     attacker_square = enemy_king_attackers.pop()
                     if chess.Move(attacker_square, enemy_king_square) in move_actions:
+                        print("*Tried to kill the king*")
                         return chess.Move(attacker_square, enemy_king_square)
 
-        # get the most popular move among all board states
+        # if we weren't under attack and couldn't kill the king
         move_counts = Counter()
         for state in self.board_states:
             board = chess.Board(state)
             board.turn = self.color
             try:
+                # find stockfish's best move across each state
                 result = self.engine.play(board, chess.engine.Limit(time=time_limit))
                 if (result.move in move_actions):
                     move_counts[result.move] += 1
@@ -341,9 +323,11 @@ class ImprovedAgent(Player):
                 pass
 
         if move_counts:
+            print("*Chose stockfish's most popular move*")
+            # return the most popular move across all states
             return max(move_counts, key=move_counts.get)
 
-        # if all else fails, pass
+        # if all else fails, just choose random
         return random.choice(move_actions)
 
     def handle_move_result(self, requested_move: Optional[chess.Move], taken_move: Optional[chess.Move],
